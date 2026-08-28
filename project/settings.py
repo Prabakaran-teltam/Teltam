95ebcad74a6d121918236dff656307e502acd2f4
Yolo-DLK <yolo27552@gmail.com>
2026-08-28 14:04:17 +0530

    my last commit

--- project/settings.py
+++ project/settings.py
@@ -88,9 +88,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'teltam_uat_db'), 
+        'USER': os.environ.get('POSTGRES_USER', 'teltam_uat_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'teltamuat'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
